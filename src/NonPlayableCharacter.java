85f6ebd1312da04a191c7edc1e95c64ab649e66d
Dominik Prengel <dominik.prengel@eviden.com>
2025-02-10 12:08:50 +0100

    Waffen Hinzugefügt

--- src/NonPlayableCharacter.java
+++ src/NonPlayableCharacter.java
@@ -1,7 +1,7 @@
 public class NonPlayableCharacter extends Character{
     private boolean hostile;
-    public NonPlayableCharacter(int life, int strength, int armourclass, int speed, int dexterity, int constitution, int intelligence, int wisdom, int charisma, boolean hostile) {
-        super(life, strength, armourclass, speed, dexterity, constitution, intelligence, wisdom, charisma);
+    public NonPlayableCharacter(int life, int strength, int armourclass, int speed, int dexterity, int constitution, int intelligence, int wisdom, int charisma, boolean hostile, Weapon weapon1, Weapon weapon2) {
+        super(life, strength, armourclass, speed, dexterity, constitution, intelligence, wisdom, charisma, weapon1, weapon2);
         this.hostile = hostile;
     }
 

6f751a13fd200bf7bdd39d4caf9b4c6e66494737
Marcus <plitt.marcus@gmail.com>
2025-02-12 14:17:50 +0100

    Änderung "life" in "max_health" und "current_health"

--- src/Character.java
+++ src/Character.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Character {
 
-    private int life;
+    private int max_health;
+    private int current_health;
     private int strength;
     private int armourclass;
     private int speed;
@@ -10,12 +11,13 @@ public class Character {
     private int constitution;
     private int intelligence;
     private int wisdom;
-    private int charisma;
+    private int charism;
     private ArrayList<Weapon> weapons;
 
     public Character(int life, int strength, int armourclass, int speed, int dexterity, int constitution, int intelligence, int wisdom, int charisma, Weapon weapon1, Weapon weapon2){
 
-        this.life = life;
+        this.max_health = life;
+        this.current_health = life;
         this.strength = strength;
         this.armourclass = armourclass;
         this.speed = speed;
@@ -23,7 +25,7 @@ public class Character {
         this.constitution = constitution;
         this.intelligence = intelligence;
         this.wisdom = wisdom;
-        this.charisma = charisma;
+        this.charism = charisma;
         this.weapons.add(0,weapon1);
         this.weapons.add(1, weapon2);
     }
@@ -36,7 +38,7 @@ public class Character {
     public void Attack (Character attacker, Character defender, int weapon){
         int attack = Roll_Dice(20) + attacker.strength;
         if (attack > defender.armourclass) {
-            defender.life -= Roll_Dice(attacker.weapons.get(weapon).getWeaponDice()) + attacker.weapons.get(weapon).getWeaponDmg();
+            defender.current_health -= Roll_Dice(attacker.weapons.get(weapon).getWeaponDice()) + attacker.weapons.get(weapon).getWeaponDmg();
         }
     }
 }
